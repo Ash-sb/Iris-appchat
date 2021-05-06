9cbeae2c516c431153142590f5766c826b29643d
Laulaulmg <laurine.laminger@hetic.net>
2021-05-06 02:15:30 +0200

    messages d'erreurs v3

--- pages/register.php
+++ pages/register.php
@@ -17,13 +17,23 @@
             $content .= "Saisir votre nom entre 3 et 20 caractères.<br>";
         } else if (1 !== preg_match('~^[a-zA-Z0-9_-]+$~', $name)) {
             $content .= "Votre nom ne peut contenir que des caractères non accentués, des chiffres, tirets et underscores.";
+        } else if (preg_match('/[a-z]/', $mdp)) {
+            $content .= "Veuillez saisir un mot de passe avec une lettre minuscule.<br>";
+        } else if (preg_match('/[A-Z]/', $mdp)) {
+            $content .= "Veuillez saisir un mot de passe avec une lettre majuscule.<br>";
+        } else if (preg_match('/[0-9]/', $mdp)) {
+            $content .= "Veuillez saisir un mot de passe avec un chiffre.<br>";
+        } else if (preg_match('/[%!?*]/', $mdp)) {
+            $content .= "Veuillez saisir un mot de passe avec un caractère spécial [%!?*].<br>";
+        } else if (iconv_strlen($mdp) < 10 || iconv_strlen($mdp) > 20) {
+            $content .= "Veuillez saisir un mot de passe entre 10 et 20 caractères.<br>";
         } else{
             register($name, $email, $password);
             $_SESSION['tchat'] = $email;
             header("Location:index.php?page=membres");
         }
 
-    }
+        }
 
 ?>
 
